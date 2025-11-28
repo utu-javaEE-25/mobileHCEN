5ba1fbb5d07ed43a43e8a47713c9809fc903790f
bentanic-0 <bentanico@gmail.com>
2025-11-28 04:40:35 -0300

    Add new tab for 'Historia Clínica' and improve tab icon formatting

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -15,19 +15,38 @@ export default function TabLayout() {
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
         tabBarButton: HapticTab,
-      }}>
+      }}
+    >
+      {/* Home */}
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
         }}
       />
+
+      {/* Explore */}
       <Tabs.Screen
         name="explore"
         options={{
           title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="paperplane.fill" color={color} />
+          ),
+        }}
+      />
+
+      {/* Historia Clínica */}
+      <Tabs.Screen
+        name="historia-clinica/index"
+        options={{
+          title: 'Historia Clínica',
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="doc.text.magnifyingglass" color={color} />
+          ),
         }}
       />
     </Tabs>
